49be054711a96531cf4fe70c089a773879877f8a
Yeol Choi <chlduf153@gmail.com>
2022-02-06 19:45:06 +0900

    accounts 마이그레이션 오류 해결(db 삭제 후 다시, createsuperuser해야함)

--- metabusDjango/settings.py
+++ metabusDjango/settings.py
@@ -167,7 +167,7 @@ INTERNAL_IPS = ['127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentications.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
