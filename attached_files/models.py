0ce7288bf669179938c38205c5e4c0440944341d
Yeol Choi <chlduf153@gmail.com>
2022-02-07 21:55:04 +0900

    attached_files 모델명 변경

--- attached_files/models.py
+++ attached_files/models.py
@@ -10,7 +10,7 @@ class TimestampedModel(models.Model):
         abstract = True
 
 
-class Attached_file(TimestampedModel):
+class AttachedFile(TimestampedModel):
     notice_no = models.ForeignKey(Notice, on_delete=models.CASCADE)
     att_file_no = models.IntegerField(max_length=100, db_index=True, blank=False)
     att_file = models.TextField(blank=False)
