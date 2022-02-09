54391474bb35e8724fee0738b9c4c09d6a3834e6
Yeol Choi <chlduf153@gmail.com>
2022-02-09 14:26:51 +0900

    입양후기 시리얼라이저 변경

--- adopt_review/serializers.py
+++ adopt_review/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
-from accounts.models import User
 from adopt_review.models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ["review_no", "nickname", "title", "content", "image", "user"]
+        fields = ["review_no", "title", "content", "image", "user"]
 
