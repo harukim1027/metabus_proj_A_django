fc262ab6ed9d846e64239340441dee7402021c6c
Yeol Choi <chlduf153@gmail.com>
2022-02-18 11:38:12 +0900

    streetanimal admin category 표시되도록 변경

--- streetanimal/admin.py
+++ streetanimal/admin.py
@@ -4,7 +4,7 @@ from streetanimal.models import Animal, Category
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
-    list_display = ["animal_no", "animal_reg_num", "sex", "date_of_discovery", "start_date", "end_date"]
+    list_display = ["animal_no", "animal_reg_num", "category", "sex", "date_of_discovery", "start_date", "end_date"]
     list_display_links = ["animal_reg_num"]
     search_fields = ["animal_reg_num"]
 
