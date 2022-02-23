e559a39b5f8fe96261e2a7bc91c8228d6fc48a43
Yeol Choi <chlduf153@gmail.com>
2022-02-23 14:28:28 +0900

    streetanimal 페이징뷰/낫페이징뷰 나누기

--- streetanimal/urls.py
+++ streetanimal/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from streetanimal.views import AnimalViewSet
-
+from streetanimal.views import AnimalViewSet, AnimalPageViewSet
 
 app_name = 'streetanimal'
 
 router = DefaultRouter()
-router.register("animal", AnimalViewSet)
+router.register("animalnotpaging", AnimalViewSet)
+router.register("animal", AnimalPageViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
--- streetanimal/views.py
+++ streetanimal/views.py
@@ -6,9 +6,34 @@ from streetanimal.models import Animal
 from streetanimal.serializers import AnimalSerializer, AnimalCreateSerializer
 
 
-class AnimalViewSet(viewsets.ModelViewSet):
+class AnimalPageViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
     pagination_class = Pagination
+
+    def get_serializer_class(self):
+        method = self.request.method
+        if method == "GET":
+            return AnimalSerializer
+        return AnimalCreateSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        query = self.request.query_params.get("query", "")
+        if query:
+            qs = qs.filter(animal_reg_num__icontains=query)
+
+        return qs
+
+    def get_permissions(self):
+        method = self.request.method
+        if method == "GET" or method == "PATCH":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+
+class AnimalViewSet(viewsets.ModelViewSet):
+    queryset = Animal.objects.all()
 
     def get_serializer_class(self):
         method = self.request.method
