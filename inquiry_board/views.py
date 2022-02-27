8f88b0bc3c06754bfba52e334f150325cdfd9e80
Yeol Choi <chlduf153@gmail.com>
2022-02-27 16:33:40 +0900

    1:1 문의 author 쿼리필드 추가

--- inquiry_board/views.py
+++ inquiry_board/views.py
@@ -21,7 +21,11 @@ class InquiryViewSet(viewsets.ModelViewSet):
 
         query = self.request.query_params.get("query", "")
         if query:
-            qs = qs.filter(title__icontains=query) or qs.filter(inquiry_no__icontains=query) or qs.filter(user__userID__icontains=query)
+            qs = qs.filter(title__icontains=query) or qs.filter(inquiry_no__icontains=query) or qs.filter(user__icontains=query)
+
+        author = self.request.query_params.get("author", "")
+        if author:
+            qs = qs.filter(user__userID__exact=author)
 
         return qs
 
