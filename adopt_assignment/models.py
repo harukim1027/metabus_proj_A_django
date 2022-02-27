b80c69450959ccb0a154d725d1eb89ffdd7b77e4
Yeol Choi <chlduf153@gmail.com>
2022-02-27 20:28:46 +0900

    입양신청 신청날짜 Datefield로 변경 + 마이그레이션

--- adopt_assignment/models.py
+++ adopt_assignment/models.py
@@ -14,8 +14,8 @@ def validate_image(image):
 
 
 class TimestampedModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     class Meta:
         abstract = True
